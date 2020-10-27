79b83acff5391bbc4e76bcfd3156a4346f5bddba
zuigzm <zuigzm@gmail.com>
2020-10-27 17:44:31 +0800

    修改获取方式

--- bin/server.js
+++ bin/server.js
@@ -1,34 +1,46 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
-const path = require('path')
-const json = path.join(__dirname, '../server.txt')
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const json = path.join(__dirname, "../server.txt");
 
 module.exports = () => {
-	return new Promise((resolve, reject) => {
-		fs.readFile(json, 'utf8', (err, data) => {
-			if (err) throw err
-			if (!data) data = []
-			data = JSON.parse(data)
-			resolve(data)
-		})
-	}).then(data => {
-		return inquirer
-			.prompt([
-				{
-					type: 'list',
-					name: 'select',
-					message: '请选择服务器',
-					choices: data
-				}
-			])
-			.then(answers => {
-				const serverList = {}
-				data.map(item => {
-					if (item.name === answers.select) {
-						Object.assign(serverList, item)
-					}
-				})
-				return serverList
-			})
-	})
-}
+  return new Promise((resolve, reject) => {
+    fs.readFile(json, "utf8", (err, data) => {
+      if (err) throw err;
+
+      if (!data) data = [];
+      data = data
+        .split("-")
+        .filter((i) => i)
+        .map((item) => {
+          if (item.length > 0) {
+            return JSON.parse(Buffer.from(item, "base64").toString("utf8"));
+          }
+        });
+
+      resolve(data);
+    });
+  }).then((data) => {
+    return inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "time",
+          message: "请选择服务器",
+          choices: data.map((i) => ({
+            name: i.name,
+            value: i.time,
+          })),
+        },
+      ])
+      .then((answers) => {
+        const serverList = {};
+        data.map((item) => {
+          if (item.time === answers.time) {
+            Object.assign(serverList, item);
+          }
+        });
+        return serverList;
+      });
+  });
+};
