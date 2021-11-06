d2a3018b137dee677b475803f8701a4054db8cc5
zuigzm <zuigzm@gmail.com>
2021-11-06 18:44:09 +0800

    fix: add terser

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const extensions = ['.js', '.ts'];
@@ -37,5 +38,6 @@ module.exports = {
       babelHelpers: 'runtime',
       extensions,
     }),
+    terser(),
   ],
 };
